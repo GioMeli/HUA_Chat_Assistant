56105f85e60285e2abbbe2a206b576dd9fc2b6ef
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-16 21:42:05 +0200

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const { register, login, logout } = require('../controllers/authController');
-const authMiddleware = require('../middleware/auth');
+const express = require("express");
+const { register, login } = require("../controllers/authController");
 
 const router = express.Router();
 
-// User authentication routes
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', authMiddleware, logout);
+router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
