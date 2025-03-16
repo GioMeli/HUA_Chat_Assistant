c8a130f473ec066f9d742f609918707fbcc271bb
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-16 15:43:34 +0200

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,9 +4,9 @@ const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
 
+// User authentication routes
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', authMiddleware, logout);
 
 module.exports = router;
-
