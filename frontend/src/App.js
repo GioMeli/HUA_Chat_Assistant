fbd5293c0b5108e779894ce8b96c6b16730307d2
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-16 21:59:29 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from "react";
-import Home from "./pages/Home";
 import SignIn from './components/SignIn';
 import Register from './components/Register';
 import Chat from './components/Chat';
@@ -9,7 +8,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/" element={<SignIn />} />
         <Route path="/register" element={<Register />} />
         <Route path="/chat" element={<Chat />} />
