7025f5a2e4c891fd05fe7d75e0bcb44c8d80a274
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-16 22:04:38 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignIn from "./components/SignIn";  // Import SignIn
+import SignIn from "./pages/SignIn";  // Import SignIn
 import Register from "./components/Register";
 import Chat from "./components/Chat";
 
